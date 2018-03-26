1f6a4c391a5ec9ca0ca04375cc54063c00fe6d32
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-03-26 10:13:00 -1000

    secret

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,9 @@
 'use strict';
 module.exports = function() {
-  
-};
+        let secretNum = Math.random() * 1000000;
+        return function another() {
+            return secretNum;
+        }
+    var secretNumber = randomNum();
+    var anotherSecret = randomNum();
+}
